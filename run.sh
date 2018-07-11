ecfa2af526fa1c9190010d70b9aeaebfbdcaf2c8
feedmenoise <feedmenoise@list.ru>
2018-07-11 18:54:38 +0300

    Fixed script for Crontab

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 DATE=`date +%Y-%m-%d`
 
-cat /var/log/asterisk/cdr-csv/Master.csv | grep $DATE > temp
+cat /var/log/asterisk/cdr-csv/Master.csv | grep $DATE > /PATH_TO_JAR/temp
 ITER=`cat /var/log/asterisk/cdr-csv/Master.csv | grep $DATE | wc -l`
-
+cd /PATH_TO_JAR/
 java -jar AsteriskLog.jar temp $ITER
-rm -rf temp
-rm -rf temp.complete
+sleep 2
+java
